b283158ff838512362e53a3c87a4901e98ddc2eb
mirayanali5 <mirayanali5@gmail.com>
2025-06-16 02:10:23 +0530

    update

--- 3/read_data.py
+++ 3/read_data.py
@@ -1,13 +1,12 @@
 import xml.etree.ElementTree as ET 
 def get_user_details(user_id): 
-    tree = ET.parse(r'C:\Users\') 
+    tree = ET.parse(r'C:\Users\miray\OneDrive\Desktop\WT-Lab\3\user.xml') 
     root = tree.getroot() 
     for user in root.findall('user'): 
         if user.get('id') == user_id: 
             name = user.find('name').text 
             email = user.find('email').text 
-            address = user.find('address').text 
-            return f"Name: {name}, Email: {email}, Address: {address}" 
+            return f"Name: {name}, Email: {email}" 
     return "User not found" 
 user_id = input("Enter a user ID: ") 
 print(get_user_details(user_id)) 
