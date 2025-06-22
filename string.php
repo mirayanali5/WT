77552fd24ce16ad0a19020ad929cb630ebd552cf
Mir Ayan Ali <142529805+mirayanali5@users.noreply.github.com>
2025-06-22 18:15:51 +0530

    Update string.php

--- string.php
+++ string.php
@@ -1,16 +1,11 @@
 <?php
-// Input string
 $inputString = "Hello, this is a sample string for PHP string manipulation.";
-// a. Find the length of a string
 $length = strlen($inputString);
 echo "Length of the string: " . $length . "\n";
-// b. Count the number of words in a string
 $wordCount = str_word_count($inputString);
 echo "Number of words in the string: " . $wordCount . "\n";
-// c. Reverse a string
 $reversedString = strrev($inputString);
 echo "Reversed string: " . $reversedString . "\n";
-// d. Search for a specific string
 $searchString = "sample";
 $position = strpos($inputString, $searchString);
 if ($position !== false) {
